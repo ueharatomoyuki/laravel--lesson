b4067eeea2625104eb4e48d82a80c78cd33ad110
tc-kishimoto <keisuke.kishimoto@common-create.com>
2022-05-24 12:23:42 +0900

    SQL修正

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,4 @@
+-- drop table users; 
 create table users(
     id serial primary key
     , login_id varchar(255) not null unique
@@ -8,6 +9,7 @@ create table users(
     , updated_at timestamp
 );
 
+-- drop table categories;
 create table categories(
     id serial primary key
     , name varchar(255) not null
@@ -15,12 +17,14 @@ create table categories(
     , updated_at timestamp
 );
 
+-- drop table products; 
 create table products(
     id serial primary key
-    , product_id int not null unique 
+    , product_id varchar(20) unique not null 
     , category_id int not null
     , name varchar(255) not null
     , price int 
+    , image_path varchar(255)
     , description varchar(2000)
     , created_at timestamp
     , updated_at timestamp
