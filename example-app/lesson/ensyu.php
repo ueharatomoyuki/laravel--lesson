09b295d577035fab4d9346783c97fdd2ebbce05b
uehara tomoyuki <22uehara_tomoyuki@axiz.co.jp>
2022-06-06 17:41:56 +0900

    dev

--- example-app/lesson/ensyu.php
+++ example-app/lesson/ensyu.php
@@ -8,4 +8,12 @@
 9  $student = DB::table('students')->where('student_id',10)->update(['grade'=>3]);
 
 10 $student = DB::table('students')->where('student_id','=',10)->delete();
+
+//発展
+
+6 sales::where('amount','<','5000')->get();
+
+7 sales::where('amount','>=','5000')->get();
+
+8 DB::select("select sales_id,order_date,customer_id,amount*1.1 from sales");
 ?>
